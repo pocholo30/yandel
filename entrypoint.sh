60c8d1ae48c45c05f9b3459e0731d52a401302fb
pocholo30 <145801208+pocholo30@users.noreply.github.com>
2025-09-05 18:32:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,9 +48,9 @@ function run_with_retries() {
 
 echo "==================="
 
-#git config --global user.name "${GITHUB_ACTOR}"
-#git config --global user.email "${INPUT_EMAIL}"
-#git config --global --add safe.directory /github/workspace
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
 	
 #python3 /usr/bin/feed.py
 
